a9fed359e26ab1197f409e5cf51df0bc272e11d0
TF-tanvir <tanvirkse@gmail.com>
2023-03-16 14:53:54 +0600

    updated

--- src/app/api/referencesEn.ts
+++ src/app/api/referencesEn.ts
@@ -4,14 +4,12 @@ export const referencesEn = [
         personFullname: 'Dr. Abu Sayed Md. Latiful Haque',
         personPosition: "Professor & CEO",
         personCompany: 'eSRD-Lab',
-        message: "Zinedine completed her internship in my company. He is independent and has a great learning ability. He meets delivery deadlines. He has team spirit. Zinedine does not hesitate to propose new relevant ideas. He respects the instructions, its code is clean.",
-    },
+        message: "I had the pleasure of working with Tanvir Ahamed for 1 year and 5 months, and I must say that he was one of the best team members we had during that time. Tanvir consistently demonstrated a strong work ethic, delivering tasks on time and to a high standard. He was always willing to take on new challenges, and his problem-solving ability was exceptional. What really stood out about Tanvir was his communication skills. He was able to effectively communicate with team members and clients alike, and was always willing to go the extra mile to ensure that everyone was on the same page. Tanvir's ability to adapt to changing project requirements and deadlines was also a great asset to our team. I highly recommend Tanvir and am confident that he will continue to excel in his future endeavors."},
     {
         personImg: "assets/images/mohamed-yahiatene.jpg",
         personFullname: 'Sazzad Hossain',
         personPosition: "Professor",
         personCompany: 'North South University',
-        message: "I worked for two years with Zinedine at the University of Lille. Zinedine is a very good working partner with high knowledge in backend and frontend development, he is always keen to move forward without ever being afraid to use new technologies. He does not hesitate to help his colleagues whenever he can. He manages to solve problems quickly and efficiently with typical optimism.",
-    }
+        message: "I have had the pleasure of teaching Tanvir Ahamed for multiple courses, including Artificial Intelligence, Machine Learning, and Senior Projects. Tanvir is an excellent student who has consistently demonstrated a strong work ethic and a keen desire to learn. He has a natural aptitude for programming and has consistently delivered high-quality work, both individually and as a member of a team. Tanvir's ability to grasp complex concepts and apply them to real-world problems is truly impressive. He has shown a particular talent for developing innovative solutions that are both practical and effective. His strong analytical skills, attention to detail, and ability to work independently make him an asset to any team. I am confident that Tanvir will continue to excel in his future endeavors, and I highly recommend him to any potential employer."}
 ];
 
