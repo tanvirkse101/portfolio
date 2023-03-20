890c61d59886a82fb705a508d4c64757d79cab7b
tanvirkse101 <tanvirkse@gmail.com>
2023-03-20 20:18:38 +0600

    auto scroll if inactive update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,12 +24,34 @@ export class AppComponent implements OnInit {
     let lang:any=localStorage.getItem("lang"); 
     this._translateService.setDefaultLang(lang);
     this._translateService.use(lang);
-    setTimeout(() => {
-      const section = this.elementRef.nativeElement.querySelector('#about');
-      if (section) {
-        section.scrollIntoView({ behavior: 'smooth', block: 'start', inline: 'nearest' });
-      }
-    }, 3500);
-  }
+    // setTimeout(() => {
+    //   const section = this.elementRef.nativeElement.querySelector('#about');
+    //   if (section) {
+    //     section.scrollIntoView({ behavior: 'smooth', block: 'start', inline: 'nearest' });
+    //   }
+    // }, 3500);
+    let inactiveTime = 0;
 
+    document.addEventListener('mousemove', function() {
+      inactiveTime = 0;
+    });
+
+    document.addEventListener('keydown', function() {
+      inactiveTime = 0;
+    });
+
+    const scrollTimeout = () => {
+      if (inactiveTime >= 4000) {
+        const section = this.elementRef.nativeElement.querySelector('#about');
+        if (section) {
+          section.scrollIntoView({ behavior: 'smooth', block: 'start', inline: 'nearest' });
+        }
+      } else {
+        inactiveTime += 1000;
+        setTimeout(scrollTimeout, 1000);
+      }
+    };
+
+    setTimeout(scrollTimeout, 3500);
+  }
 }
